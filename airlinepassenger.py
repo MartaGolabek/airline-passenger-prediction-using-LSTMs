ef37ac369829d95d16150613f2b282f5d5aabbd9
Marta <mgolabek@inovex.de>
2020-04-05 00:35:07 +0200

    Univariate approach

--- airlinepassenger.py
+++ airlinepassenger.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
         raw_data_set = pd.read_csv('data/raw/international-airline-passengers.csv')
         interim_data_set = mid.initial_data_preprocessing(raw_data_set)
         with open('data/interim/international-airline-passengers.csv', 'w') as outfile:
-            interim_data_set.to_csv(outfile, header=True)
+            interim_data_set.to_csv(outfile, header=True, index=False)
         print("Initial preprocessing has been completed.")
 
     # Univariate Approach
     univariate_df = interim_data_set["passenger_number"]
-    
+
 
     # Multivariate Approach
